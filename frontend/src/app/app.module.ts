d0d7d272e0c9ce1bc75b3b35f3571f7694ec4bc1
viniciusleitempergher <viniciusgabriel.com@gmail.com>
2022-04-11 09:15:10 -0300

    componente navbar criado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Pg404Component } from './pg404/pg404.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Pg404Component
+    Pg404Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
